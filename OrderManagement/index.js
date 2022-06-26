0315f651d9002730ea50f3e416d56c2df8474ab8
aeshtech <dev.aeshtech@gmail.com>
2022-06-26 14:18:09 +0530

    Added OrderManagement

---
+++ OrderManagement/index.js
@@ -0,0 +1,51 @@
+const orderIdInfo = document.getElementById('order-id-info')
+let statusBlocks = document.getElementsByClassName('status-block')
+
+const addDoneClass = (index) => statusBlocks[index].classList.add('done')
+
+const executeOrder = (callback) => {
+	const orderId = document.getElementById('order-id-input').value
+	orderIdInfo.innerText = 'Status for Order ID: ' + orderId
+	let x = 0
+	addDoneClass(x++)
+
+	setTimeout(() => {
+		addDoneClass(x++)
+
+		setTimeout(() => {
+			addDoneClass(x++)
+
+			setTimeout(() => {
+				addDoneClass(x++)
+
+				setTimeout(() => {
+					addDoneClass(x++)
+
+					setTimeout(() => {
+						addDoneClass(x++)
+						
+						setTimeout(() => {
+							addDoneClass(x++)
+
+							setTimeout(() => {
+								addDoneClass(x++)
+
+								setTimeout(() => {
+									addDoneClass(x++)
+									callback()
+								}, 2000)
+
+							}, 8000)
+
+						}, 15000)
+
+					}, 5000)
+
+				}, 12000)
+
+			}, 5000)
+
+		}, 10000)
+		
+	}, 2000)
+}
